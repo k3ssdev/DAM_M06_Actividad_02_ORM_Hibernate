0b2d8a9b59a3fb1b2e0641931d925e496f34aee7
k3ssdev <albertoperezdelrio@gmail.com>
2023-03-13 21:34:16 +0100

    update

--- notaslinkia_Alberto_Perez_del_Rio.sql
+++ notaslinkia_Alberto_Perez_del_Rio.sql
@@ -41,17 +41,6 @@ CREATE TABLE `alumnos` (
   `password` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Volcado de datos para la tabla `alumnos`
---
-
-INSERT INTO `alumnos` (`id_alumno`, `nombre`, `nom_user`, `password`) VALUES
-(1, 'Maria', 'maria2', 'clave1234'),
-(2, 'Luis García', 'luis', 'clave3'),
-(3, 'Ana Martínez', 'ana', 'clave4'),
-(4, 'Pedro Picapiedra', 'pica', '12345'),
-(5, 'Sandra Gomez', 'sandra', '12345');
-
 
 -- --------------------------------------------------------
 
@@ -66,14 +55,7 @@ CREATE TABLE `historial` (
   `detalle` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Volcado de datos para la tabla `historial`
---
 
-INSERT INTO `historial` (`id`, `tipo`, `user`, `detalle`) VALUES
-(1, 'Inicio de sesión', 5, '2023-03-12 20:32:38.158'),
-(2, 'Inicio de sesión', 5, '2023-03-12 20:39:53.159'),
-(3, 'Inicio de sesión', 10, '2023-03-12 20:39:53.159');
 -- --------------------------------------------------------
 
 --
@@ -84,26 +66,6 @@ CREATE TABLE `modulos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Volcado de datos para la tabla `modulos`
---
-
-INSERT INTO `modulos` (`id`, `nombre`) VALUES
-(1, 'M01'),
-(2, 'M02'),
-(3, 'M03'),
-(4, 'M04'),
-(5, 'M05'),
-(6, 'M06'),
-(7, 'M07'),
-(8, 'M08'),
-(9, 'M09'),
-(10, 'M10'),
-(11, 'M11'),
-(12, 'M12'),
-(13, 'M13'),
-(14, 'M14');
 
 
 -- --------------------------------------------------------
@@ -119,23 +81,7 @@ CREATE TABLE `notas` (
   `notas` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Volcado de datos para la tabla `notas`
---
 
-INSERT INTO `notas` (`id_notas`, `id_alumno`, `id_modulo`, `notas`) VALUES
-(1, 1, 1, 8.5),
-(2, 1, 2, 7.2),
-(3, 1, 3, 9.1),
-(4, 2, 1, 7.8),
-(5, 2, 2, 6.5),
-(6, 2, 3, 8.2),
-(7, 3, 1, 7.1),
-(8, 3, 2, 8.9),
-(9, 3, 3, 6.7),
-(10, 4, 1, 6.9),
-(11, 4, 2, 7.6),
-(12, 4, 3, 8.3);
 
 -- --------------------------------------------------------
 
@@ -151,17 +97,7 @@ CREATE TABLE `profesores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Volcado de datos para la tabla `profesores`
---
--- Importante que exista usuario
--- (1, 'Administrador', 'admin', 'admin'),
 
-INSERT INTO `profesores` (`id`, `nombre`, `nom_user`, `password`) VALUES
-(1, 'Administrador', 'admin', 'admin'),
-(2, 'Pedro Sánchez', 'pedro', 'clave1'),
-(3, 'Laura Gómez', 'laura', 'clave2'),
-(4, 'Miguel Álvarez', 'miguel', 'clave3'),
-(5, 'Sofía Ruiz', 'sofia', 'clave4');
 --
 -- Índices para tablas volcadas
 --
@@ -206,31 +142,31 @@ ALTER TABLE `profesores`
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id_alumno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `historial`
 --
 ALTER TABLE `historial`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=404;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `modulos`
 --
 ALTER TABLE `modulos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `notas`
 --
 ALTER TABLE `notas`
-  MODIFY `id_notas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id_notas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- AUTO_INCREMENT de la tabla `profesores`
 --
 ALTER TABLE `profesores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 --
 -- Restricciones para tablas volcadas
@@ -243,6 +179,81 @@ ALTER TABLE `notas`
   ADD CONSTRAINT `alumno_fk` FOREIGN KEY (`id_alumno`) REFERENCES `alumnos` (`id_alumno`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `modulo_fk` FOREIGN KEY (`id_modulo`) REFERENCES `modulos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
+
+
+
+-- Volcado de datos para la tabla `profesores`
+--
+-- Importante que exista usuario
+-- (1, 'Administrador', 'admin', 'admin'),
+
+INSERT INTO `profesores` (`id`, `nombre`, `nom_user`, `password`) VALUES
+(1, 'Administrador', 'admin', 'admin'),
+(2, 'Pedro Sánchez', 'pedro', 'clave1'),
+(3, 'Laura Gómez', 'laura', 'clave2'),
+(4, 'Miguel Álvarez', 'miguel', 'clave3'),
+(5, 'Sofía Ruiz', 'sofia', 'clave4');
+
+--
+-- Volcado de datos para la tabla `notas`
+--
+
+INSERT INTO `notas` (`id_notas`, `id_alumno`, `id_modulo`, `notas`) VALUES
+(1, 1, 1, 8.5),
+(2, 1, 2, 7.2),
+(3, 1, 3, 9.1),
+(4, 2, 1, 7.8),
+(5, 2, 2, 6.5),
+(6, 2, 3, 8.2),
+(7, 3, 1, 7.1),
+(8, 3, 2, 8.9),
+(9, 3, 3, 6.7),
+(10, 4, 1, 6.9),
+(11, 4, 2, 7.6),
+(12, 4, 3, 8.3);
+
+--
+-- Volcado de datos para la tabla `modulos`
+--
+
+INSERT INTO `modulos` (`id`, `nombre`) VALUES
+(1, 'M01'),
+(2, 'M02'),
+(3, 'M03'),
+(4, 'M04'),
+(5, 'M05'),
+(6, 'M06'),
+(7, 'M07'),
+(8, 'M08'),
+(9, 'M09'),
+(10, 'M10'),
+(11, 'M11'),
+(12, 'M12'),
+(13, 'M13'),
+(14, 'M14');
+
+
+--
+-- Volcado de datos para la tabla `historial`
+--
+
+INSERT INTO `historial` (`id`, `tipo`, `user`, `detalle`) VALUES
+(1, 'Inicio de sesión', 5, '2023-03-12 20:32:38.158'),
+(2, 'Inicio de sesión', 5, '2023-03-12 20:39:53.159'),
+(3, 'Inicio de sesión', 10, '2023-03-12 20:39:53.159');
+
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id_alumno`, `nombre`, `nom_user`, `password`) VALUES
+(1, 'Maria', 'maria2', 'clave1234'),
+(2, 'Luis García', 'luis', 'clave3'),
+(3, 'Ana Martínez', 'ana', 'clave4'),
+(4, 'Pedro Picapiedra', 'pica', '12345'),
+(5, 'Sandra Gomez', 'sandra', '12345');
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
